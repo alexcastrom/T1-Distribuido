bc761b9c3125a970a2f161286482be860b05e932
Manditar2 <gabriel.castillo2@mail.udp.cl>
2023-04-11 18:50:19 -0400

    Maybe esto

--- server/server.js
+++ server/server.js
@@ -2,43 +2,57 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
-const yaml = require('yaml');
 const path = require('path');
-const  crearEsquema = require('./esquemachuck.js')
+const crearEsquema = require('./esquemachuck.js')
 
 const url_1 = process.env.REDIS_URL_1
 const port = process.env.PORT || 3000;
 
-require('dotenv').config()  
-
-let esquemaChuck
-
-async function crearIndexar(){
-  const esquema = await crearEsquema(url_1)
-  esquemaChuck = esquema
-}
-crearIndexar()
-
-app.use(cors())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+require('dotenv').config()
 
 
-// RUTAS // 
+//---------------------------------- Acá empieza el código -------------------------------------------------------------
 
-const ruta_principal = path.join(__dirname, '..','index.html')
+;(async function() {
+  try {
+    const esquemaChuck = await crearEsquema(url_1)
+    app.use(cors())
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    const ruta_principal = path.join(__dirname, '../public', 'index.html')
 
-app.get('/',function(req,res){
-  res.sendFile(ruta_principal)
-})
+// --------------------- Rutas ------------------------------------------
 
-app.post('/buscar',function(req,res){
+    app.get('/', function (req, res){res.sendFile(ruta_principal)})
 
-})
+    app.use('/buscar',  async function (req, res, next) {
+      console.log(req.body.palabra)
+      try {
+        const buscarFrase = await esquemaChuck.fetch(req.body.palabra)
+        res.send(buscarFrase)
+        next()
+      }
+      catch (error) {
+        console.log(error)
+      }
+    })
 
-app.listen(port, function() {
-    console.log(`Listening on port ${port}`);
-    console.log(process.env['REDIS_URL_1'])
-    console.log(esquemaChuck)
-  });
-  
+    app.post('/buscar', function (req, res) {
+      console.log("Inicio, buscando o llamando a la API")
+      console.log(req.body)
+      res.json({ palabra: req.body.palabra })
+    })
+
+
+
+
+// ------------------------------------------ Estándar servidor -----------------------------------------------------------------
+    app.listen(port, function () {
+      console.log(`Listening on port ${port}`);
+      console.log(process.env['REDIS_URL_1'])
+      console.log(esquemaChuck)
+    });
+  } catch (error) {
+    console.log(error)
+  }
+})();
